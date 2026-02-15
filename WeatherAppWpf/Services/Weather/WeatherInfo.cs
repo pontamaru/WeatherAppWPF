d54139553cd427e1659eb47f1944774d23844588
pontamaru <best.eng.mgs@gmail.com>
2026-02-15 12:16:17 +0900

    [update]天気アイコンの表示機能追加

--- WeatherAppWpf/Services/Weather/WeatherInfo.cs
+++ WeatherAppWpf/Services/Weather/WeatherInfo.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace WeatherAppWpf.Services.Weather
+﻿namespace WeatherAppWpf.Services.Weather
 {
 	public class WeatherInfo
 	{
